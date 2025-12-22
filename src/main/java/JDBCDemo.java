ebe0f445c0e2603a5b2fa6de22adaed78f4e6677
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-22 19:26:33 +0530

    JDBC connection

---
+++ src/main/java/JDBCDemo.java
@@ -0,0 +1,8 @@
+public class JDBCDemo {
+    private static final String URL="";
+    private static final String USER ="";
+    private static final String PASSWORD="";
+    public static void main(String[] args) {
+
+    }
+}
