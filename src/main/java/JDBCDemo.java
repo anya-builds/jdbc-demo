1146fd37e12f49c93ab7f08d055c999331be9e8b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-22 22:09:30 +0530

    JDBC connection

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -1,7 +1,7 @@
 public class JDBCDemo {
-    private static final String URL="";
-    private static final String USER ="";
-    private static final String PASSWORD="";
+    private static final String URL="jdbc:mysql://localhost:3306/demo_db";
+    private static final String USER ="root";
+    private static final String PASSWORD="T9!rA#2Z@L7p$Q";
     public static void main(String[] args) {
 
     }
