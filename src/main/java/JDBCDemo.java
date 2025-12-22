7b9cedea0f4882d2a21f5c8c3aebcaffc5ff369b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-22 22:13:31 +0530

    JDBC connection

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -1,8 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCDemo {
     private static final String URL="jdbc:mysql://localhost:3306/demo_db";
     private static final String USER ="root";
     private static final String PASSWORD="T9!rA#2Z@L7p$Q";
     public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(URL , USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
