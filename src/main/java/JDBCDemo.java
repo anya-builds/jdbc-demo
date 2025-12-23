dce7a1b1e6987f34a0a852c953fc31f7e3bebff9
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-23 18:25:49 +0530

    JDBC connection

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -3,16 +3,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCDemo {
-    private static final String URL="jdbc:mysql://localhost:3306/demo_db";
-    private static final String USER ="root";
-    private static final String PASSWORD="T9!rA#2Z@L7p$Q";
+    private static final String URL = "jdbc:mysql://localhost:3306/demo_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "T9!rA#2Z@L7p$Q";
+
     public static void main(String[] args) {
+        Connection conn = null;
         try {
-            Connection conn = DriverManager.getConnection(URL , USER, PASSWORD);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected to the database!");
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
+        }
     }
 }
