9e9b584ec6ec3734354fff9b66dc3eff2ab77769
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-24 18:39:13 +0530

    Transactions

--- src/main/java/TransactionDemo.java
+++ src/main/java/TransactionDemo.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TransactionDemo {
     private static final String URL = "jdbc:mysql://localhost:3306/demo_db";
@@ -9,8 +7,28 @@ public class TransactionDemo {
     public static void main(String[] args) {
         try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to the database!");
+
+            //Order, OrderItems
+            // INSERT INTO ORDER
+            int orderId=insertOrder(conn,101,"Alice01",2000.0);
+            // INSERT INTO ORDER
+            insertOrderItem(conn, orderId, "Laptop01",1,2000.0);
         }catch (SQLException e){
             e.printStackTrace();
+        }
+    }
+
+    private static int insertOrder(Connection conn, int customerId, String customerName, double price) {
+        String sql = "INSERT INTO order_items (order_id, product_name, quantity, price) " +
+                "VALUES (?, ?, ?, ?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            pstmt.setInt(1,customerId);
+            pstmt.setString(2,customerName);
+            pstmt.setDouble(3,price);
+            int rows = pstmt.executeUpdate();
+            System.out.println("INSERTED into orders: "+ rows);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
         }
     }
 }
